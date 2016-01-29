a749b33b4992bd2dfff5e67c8d993029a81084df
Lucky Sahani <luckythegreat4@gmail.com>
2016-01-29 14:17:16 +0530

    Classification for Naive Bayes complete

--- question1/1a.py
+++ question1/1a.py
@@ -10,9 +10,9 @@ import os
 from sklearn import cross_validation
 
 word_dictionary = {}
-test_directory =""
 output = ()
-final_data = []
+final_training_dataset = []
+final_testing_dataset = []
 
 def read_from_file(file_name_with_path,file_name):
 	temp_dictionary = {}
@@ -42,27 +42,33 @@ def read_from_file(file_name_with_path,file_name):
 
 
 def traverse_over_files():
-	global final_data
+	global final_training_dataset
 	number_of_dir_read = 0
 	print "Your current working directory is :"+os.getcwd()
 	dataset_directory = os.getcwd()+"/bare"
 	print "Your dataset directory is :"+ dataset_directory
 	get_list_of_all_subdirectories =  [dirs for root, dirs, files in os.walk(dataset_directory)]
 	for dir_cur in get_list_of_all_subdirectories[0]:
-		if(number_of_dir_read == 9):
-			global test_directory
+		if(number_of_dir_read ==  9):
 			test_directory = dataset_directory+"/"+dir_cur
-			print "Testing directory is set to :"+test_directory
-			break
-		current_directory = dataset_directory+"/"+dir_cur
-		print "Current directory is "+current_directory
-		number_of_dir_read = number_of_dir_read+1
-		for root, dirs, files in os.walk(current_directory):
-			for name in files:
-				currentFile=os.path.join(root, name)
-				# print "Reading from file "+str(currentFile)
-				output = read_from_file(currentFile,name)
-				final_data.append(output)
+			print "Current working for testing dataset on :"+test_directory
+			number_of_dir_read = number_of_dir_read+1
+			for root, dirs, files in os.walk(current_directory):
+				for name in files:
+					currentFile=os.path.join(root, name)
+					# print "Reading from file "+str(currentFile)
+					output = read_from_file(currentFile,name)
+					final_testing_dataset.append(output)
+		else:
+			current_directory = dataset_directory+"/"+dir_cur
+			print "Current working for training dataset on :"+current_directory
+			number_of_dir_read = number_of_dir_read+1
+			for root, dirs, files in os.walk(current_directory):
+				for name in files:
+					currentFile=os.path.join(root, name)
+					# print "Reading from file "+str(currentFile)
+					output = read_from_file(currentFile,name)
+					final_training_dataset.append(output)
 
 print "Reading files to make Dictionary"
 start_time = time.time()
@@ -72,16 +78,19 @@ print "It took "+ str(end_time) + " to make the dicitonary"
 print "Dictionary completed"
 print '\nTraining data'
 start_time = time.time()
-classifier = nltk.NaiveBayesClassifier.train(final_data)
+classifier = nltk.NaiveBayesClassifier.train(final_training_dataset)
 end_time = time.time() - start_time
 print "It took "+ str(end_time) + " to train the data for classifier"
 print 'Training Completed'
-print '\nTesting data on :'+test_directory
+print '\nTesting data '
 start_time = time.time()
-# See how
+for data in final_testing_dataset:
+		print "Original result : "+ data[1] + "\tClassifier result : "+classifier.classify(data[0])
 end_time = time.time() - start_time
 print "It took "+ str(end_time) + " to test the data "
 print 'Testing Completed'
 print '\nPrinting Result'
 # See what to print and how
-# classifier.show_most_informative_features(5)
+# see new nltk functions
+# remove subject word
+classifier.show_most_informative_features(5)
